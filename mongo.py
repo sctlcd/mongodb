214eb151212c12fde8ad349103ffa939db6833e5
8 <lucinda.souchet@outlook.com>
2020-07-02 19:25:49 +0100

    add command examples in mon.py

--- mongo.py
+++ mongo.py
@@ -20,7 +20,24 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-documents = coll.find()
+# Insert 1 new record
+# new_doc = {'first': 'douglas', 'last': 'adams', 'dob': '11/03/1952', 'hair_colour': 'grey', 'occupation': 'writer', 'nationality': 'english'}
+# coll.insert(new_doc)
+
+# Insert many new record
+# new_docs = [{'first': 'towa', 'last': 'Piver', 'dob': '11/03/1950', 'gender':'m',
+#     'hair_colour': 'pink', 'occupation': 'writer', 'nationality': 'english'},
+#     {'first': 'sawi', 'last': 'Sadu', 'dob': '11/03/1982', 'gender':'m',
+#     'hair_colour': 'black', 'occupation': 'writer', 'nationality': 'american'}]
+# coll.insert(new_docs)
+
+#coll.remove({'first': 'douglas'})
+
+#coll.update_one({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})
+
+coll.update_many({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})
+
+documents = coll.find({'nationality': 'american'})
 
 for doc in documents:
     print(doc)
